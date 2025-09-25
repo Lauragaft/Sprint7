693bb74ddd965a7679e27aec06dcba7a6da9e359
Lauragaft <laura.gafter@gmail.com>
2025-09-24 21:35:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')
-
+st.set_page_config(page_title="EDA Sprint 7", layout="wide")
 st.header("Análisis de anuncios de vehículos")
 
-hist_button = st.button('Construir histograma')
+car_data = pd.read_csv("vehicles_us.csv")
+
+hist_button = st.button("Construir histograma")
 
 if hist_button:
-    st.write('Creación de un histograma para la columna odómetro')
+    st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Construir gráfico de dispersión')
+scatter_button = st.button("Construir gráfico de dispersión")
 
 if scatter_button:
-    st.write('Creación de un gráfico de dispersión: precio vs odómetro')
-    fig2 = px.scatter(car_data, x="odometer", y="price")
-    st.plotly_chart(fig2, use_container_width=True)
+    st.write("Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches")
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
